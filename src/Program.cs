801ed2628562407b2d27e270db2eeff13fbfde91
osawa-koki <osawa-koki@example.com>
2023-02-22 14:39:14 +0900

    基本コードは完成した。

--- src/Program.cs
+++ src/Program.cs
@@ -1,33 +1,130 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using dotenv.net;
+using Microsoft.Extensions.Configuration;
 using MySql.Data.MySqlClient;
 
+DotEnv.Load(options: new DotEnvOptions(trimValues: true));
+var envVars = DotEnv.Read();
+var connection_string = envVars["CONNECTION_STRING"];
 
-string server = "localhost";
-string str_db = "my_db";
-string user = "root";
-string password = "p@ssword1234";
+if (connection_string == null)
+{
+  Console.WriteLine($"Unable to load `.env`.");
+  return 1;
+}
 
+var builder = new ConfigurationBuilder();
+builder.AddCommandLine(args);
+var config = builder.Build();
+var command = config["command"];
+
+if (command == null)
+{
+  Console.WriteLine($"`command` arg is null.");
+  Console.WriteLine($"Specify command listed below.");
+  Console.WriteLine($"\t- select");
+  Console.WriteLine($"\t- insert");
+  Console.WriteLine($"\t- update");
+  Console.WriteLine($"\t- delete");
+  return 1;
+}
 
 // MariaDBに接続
-MySqlConnection conn = new MySqlConnection("Server=" + server + ";Database=" + str_db + ";Uid=" + user + ";Pwd=" + password + ";");
+MySqlConnection conn = new(connection_string);
+Console.WriteLine($"Connection string -> {conn.ConnectionString}");
 
 conn.Open();
 
-// SQL文を作成
-string sql = "SELECT * FROM users";
-
-// SQL文を実行するためのコマンドを作成
-MySqlCommand cmd = new MySqlCommand(sql, conn);
-
-// データを取得
-MySqlDataReader reader = cmd.ExecuteReader();
-
-// データを表示
-while (reader.Read())
+switch (command)
 {
-    Console.WriteLine(reader["id"] + " " + reader["name"]);
+  case "select":
+    {
+      string sql = "SELECT * FROM users";
+      MySqlCommand cmd = new(sql, conn);
+      MySqlDataReader reader = cmd.ExecuteReader();
+      while (reader.Read())
+      {
+        Console.WriteLine(reader["id"] + " : " + reader["name"]);
+      }
+    }
+    break;
+  case "insert":
+    {
+      var name = config["name"];
+      if (name == null)
+      {
+        Console.WriteLine($"`name` arg is null.");
+        Console.WriteLine($"Specify name to insert.");
+        return 1;
+      }
+      string sql = "INSERT INTO users (name) VALUES (@name)";
+      MySqlCommand cmd = new(sql, conn);
+      cmd.Parameters.Add(new MySqlParameter("@name", name));
+      cmd.ExecuteNonQuery();
+    }
+    break;
+  case "update":
+    {
+      var id_str = config["id"];
+      if (id_str == null)
+      {
+        Console.WriteLine($"`id` arg is null.");
+        Console.WriteLine($"Specify name to insert.");
+        return 1;
+      }
+      if (int.TryParse(id_str, out int id) == false)
+      {
+        Console.WriteLine($"id({id}) is not integer.");
+        Console.WriteLine($"Specify `id` with integer.");
+        return 1;
+      }
+      var name = config["name"];
+      if (name == null)
+      {
+        Console.WriteLine($"`name` arg is null.");
+        Console.WriteLine($"Specify name to insert.");
+        return 1;
+      }
+      string sql = "UPDATE users SET name = @name WHERE id = @id";
+      MySqlCommand cmd = new(sql, conn);
+      cmd.Parameters.Add(new MySqlParameter("@name", name));
+      cmd.Parameters.Add(new MySqlParameter("@id", id));
+      cmd.ExecuteNonQuery();
+    }
+    break;
+  case "delete":
+    {
+      var id_str = config["id"];
+      if (id_str == null)
+      {
+        Console.WriteLine($"`id` arg is null.");
+        Console.WriteLine($"Specify name to insert.");
+        return 1;
+      }
+      if (int.TryParse(id_str, out int id) == false)
+      {
+        Console.WriteLine($"id({id}) is not integer.");
+        Console.WriteLine($"Specify `id` with integer.");
+        return 1;
+      }
+      string sql = "DELETE FROM users WHERE id = @id";
+      MySqlCommand cmd = new(sql, conn);
+      cmd.Parameters.Add(new MySqlParameter("@id", id));
+      cmd.ExecuteNonQuery();
+    }
+    break;
+  default:
+    {
+      Console.WriteLine($"`command` arg is invalid.");
+      Console.WriteLine($"Specify command listed below.");
+      Console.WriteLine($"\t- select");
+      Console.WriteLine($"\t- insert");
+      Console.WriteLine($"\t- update");
+      Console.WriteLine($"\t- delete");
+      return 1;
+    }
 }
 
 // データベースを閉じる
 conn.Close();
+
+return 0;
